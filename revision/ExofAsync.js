4184275ba06a8e691424dde941b6ba0a7bafb49e
name <rk6318633@gmail.com>
2024-07-06 23:49:09 +0530

    code added

---
+++ revision/ExofAsync.js
@@ -0,0 +1,10 @@
+console.log("start");
+
+console.log("Start executing");
+
+setTimeout(() => {
+   console.log("tasks executing from setTimeout") ;
+}, 5000); // 5000ms ,setTimeout will execute the callback after 5000ms ;
+
+
+console.log("end");
---
+++ revision/exofSync.js
@@ -0,0 +1,9 @@
+console.log("hey there");
+
+console.log("start executing");
+
+for(let i=0;i<100000000000;i++){
+   //some time consuming task;
+}
+
+console.log("end");
