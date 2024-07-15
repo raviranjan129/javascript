3c20e64d33996d79f38909b42f6fbfd11bf7b0bb
name <rk6318633@gmail.com>
2024-07-15 22:55:55 +0530

    catch demo

---
+++ revision/promise/CatchDemo03.js
@@ -0,0 +1,23 @@
+function createPromise(time){
+    return new Promise((res,rej)=>{
+        rej(time)
+    },time)
+}
+
+createPromise(10000)
+.catch(function r1(value){
+    console.log("handler first",value);
+    return value*2                                        // .catch also returns a new promise ;
+})
+.then(function r2(value){
+    console.log("first dot then",value)
+    return value*2;
+})
+.then(function g (value){
+    console.log("third dot then",value);
+    throw 600;
+})
+.catch(function r3(value){
+    console.log("error handler",value);
+    return value*2
+})
