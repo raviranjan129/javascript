132a43c37d351c0f867b21e638b02b9f40ed57c9
name <rk6318633@gmail.com>
2024-09-03 18:26:21 +0530

    conversion of datatypes

---
+++ revision/basics/conversionDatatypes.js
@@ -0,0 +1,30 @@
+
+let score=null;
+
+console.log(typeof(score))
+
+let valueInnumber=Number(score)
+console.log(typeof(valueInnumber))
+
+console.log(valueInnumber)
+
+
+//'23' -> number
+//"234abc"-> NaN
+// undefined-> NaN
+//null -> 0;
+// true->1
+//false->0
+
+let isloggedIn=1;
+
+let booleanLoggedIn=Boolean(isloggedIn);
+
+console.log(booleanLoggedIn)
+
+let someNumber = 33;
+
+let stringnumber= String(someNumber);
+
+console.log(stringnumber)
+console.log(typeof(stringnumber))
---
+++ revision/basics/dataTypes.js
@@ -0,0 +1,12 @@
+
+let name="ravi" // string
+
+let id=123; // number
+
+let userid=true;  // boolean;
+
+let email;  //undefined;
+let fruit=null; // null-> stand alone value;
+
+
+//object 
